b3be66ab20d0ccce1f453b20576e3b4aefa2fc04
Tom Levy <tom@solveit.fr>
2020-04-27 01:37:01 +0200

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
     devise_for :users
 
   root to: 'pages#home'
-  resources :questions
+    resources :questions, only: [:index, :new, :create]
 
   # Generic syntax:
   # verb 'path', to: 'controller#action'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_230549) do
+ActiveRecord::Schema.define(version: 2020_04_26_231252) do
+
+  create_table "questions", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.boolean "pour"
@@ -29,6 +36,13 @@ ActiveRecord::Schema.define(version: 2020_04_26_230549) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.boolean "pour"
+    t.boolean "contre"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
